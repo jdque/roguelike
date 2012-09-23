2ce23eea990457f7706097a5572e39cd18196ce5
Simon Huynh <shuynh27@gmail.com>
2012-09-23 02:08:04 -0500

    TileType
    Added private TileType enum for class Tile.

--- map.py
+++ map.py
@@ -8,22 +8,29 @@ class Rect:
 		self.height = height
 
 class Tile:
+    class __TileType:
+        """Private enum for class tile
+        floor = 0
+        wall = 1
+        """
+        floor = 0
+        wall = 1
 	#a tile of the map and its properties
-	def __init__(self, char, fg, bg, type):
+	def __init__(self, char, fg, bg, tileType):
 		self.char = char
 		self.fg = fg
 		self.bg = bg
-		self.type = type
-		
+		self.tileType = tileType
+
 	def set_char(self, char):
 		self.char = char
-		
+
 	def set_fg(self, fg):
 		self.fg = fg
-	
+
 	def set_bg(self, bg):
 		self.bg = bg
-		
+
 	def set_type(self, type):
 		self.type = type
 
@@ -34,22 +41,22 @@ class Map:
 		self.tiles = [[Tile(0, libtcod.Color(0,0,0), libtcod.Color(0,0,0), 0)
 			for y in range(self.height)]
 				for x in range(self.width)]
-		
+
 	def draw(self, con):
 		for y in range(self.height):
 			for x in range(self.width):
 				libtcod.console_put_char_ex(con, x, y, self.tiles[x][y].char, self.tiles[x][y].fg, self.tiles[x][y].bg)
-				
+
 	def set_tile(self, x, y, tile):
 		self.tiles[x][y].set_char(tile.char)
 		self.tiles[x][y].set_fg(tile.fg)
 		self.tiles[x][y].set_bg(tile.bg)
 		self.tiles[x][y].set_type(tile.type)
-		
+
 	def get_tile(self, x, y):
 		return self.tiles[x][y]
-		
-	def fill(self, tile):						
+
+	def fill(self, tile):
 		for y in range(self.height):
 			for x in range(self.width):
 				self.tiles[x][y].set_char(tile.char)
